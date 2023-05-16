6459b6a1e1a520852cca8816a2dd9adce54752e7
Rychu <ryszard.precikowski@gmail.com>
2023-05-16 21:26:03 +0200

    producer / consumer

--- src/consumer/consumer.py
+++ src/consumer/consumer.py
@@ -1,28 +1,33 @@
-from kafka import KafkaConsumer
 import logging
+
+from kafka import KafkaConsumer
 
 logging.basicConfig(level=logging.INFO)
 
+import time
+import random
+
 
 class Consumer:
-
-    def __init__(self, host, topic):
+    def __init__(self, host, topics):
         self.kafka_host = host
-        self.kafka_topic = topic
+        self.kafka_topic = topics
+        print(self.kafka_topic)
         self.consumer = KafkaConsumer(
-            self.kafka_topic,
-            bootstrap_servers=self.kafka_host,
+            bootstrap_servers=self.kafka_host
         )
+        self.consumer.subscribe(self.kafka_topic)
 
     def consume_from_kafka(self):
-        for message in self.consumer:
-            logging.info(message.value)
+        result = next(self.consumer)
+        logging.info(result)
+        return result
 
 
 if __name__ == "__main__":
     import sys
-    print(sys.argv)
-    consumer = Consumer(sys.argv[1], sys.argv[2])
+    consumer = Consumer(sys.argv[1], [topic for topic in sys.argv[2].split(",")])
 
     while True:
         consumer.consume_from_kafka()
+        time.sleep(random.randint(25, 75) / 100)
--- src/producer/producer.py
+++ src/producer/producer.py
@@ -1,44 +1,86 @@
 import csv
 import json
 import logging
+from dataclasses import dataclass
 
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
 
 logging.basicConfig(level=logging.ERROR)
 
+from datetime import datetime
+
+@dataclass
+class Row:
+    ts: str
+    device: str
+    co: str
+    humidity: str
+    light: str
+    lpg: str
+    motion: str
+    smoke: str
+    temp: str
+
 
 class Producer:
-    def __init__(self, host, topic):
+    def __init__(self, host):
         self.kafka_host = host
-        self.kafka_topic = topic
         self.producer = KafkaProducer(
             bootstrap_servers=self.kafka_host, value_serializer=lambda v: json.dumps(v).encode(),
         )
 
-    def publish_to_kafka(self, message):
+    def publish_to_kafka(self, message, topic):
         try:
-            self.producer.send(self.kafka_topic, message)
+            self.producer.send(topic, message)
             self.producer.flush()
 
         except KafkaError as ex:
 
             logging.error(f"Exception {ex}")
         else:
-            logging.info(f"Published message {message} into topic {self.kafka_topic}")
+            logging.info(f"Published message {message} into topic {topic}")
 
 
 if __name__ == "__main__":
     import sys
 
-    print(sys.argv)
-    producer = Producer(sys.argv[1], sys.argv[2])
-    producer.publish_to_kafka("hello")
+    producer = Producer(sys.argv[1])
+    max_idx = int(sys.argv[2])
+    idx = int(sys.argv[3])
 
+    while True:
+        with open('archive/iot_telemetry_data.csv', newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            header = next(reader, None)
 
-    with open('archive/analyst_ratings_processed.csv', newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        header = next(reader, None)
-        print("header" + str(header))
-        for row in reader:
-            producer.publish_to_kafka(row)
+            data_start = datetime(2020, 7, 12, 2, 1, 30)
+
+            i = 0
+            start = datetime.now()
+            for r in reader:
+                i += 1
+                if i % max_idx != idx:
+                    continue
+
+                row = Row(*r)
+                timestamp = datetime.fromtimestamp(float(row.ts))
+                data_delta = timestamp - data_start
+
+                while True:
+                    if (datetime.now() - start) * 100 > data_delta:
+                        break
+
+                if row.light != "false":
+                    topic = "light"
+                else:
+                    topic = "dark"
+
+                producer.publish_to_kafka(r, topic)
+
+                if float(row.temp) > 23.5:
+                    topic = "warm"
+                else:
+                    topic = "cold"
+
+                producer.publish_to_kafka(r, topic)
